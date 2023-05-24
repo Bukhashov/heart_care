9413134c729425e85e0732ef449a634ae885c551
Bukhashov <bukhashov2301@mail.ru>
2023-05-24 12:21:55 +0600

    ff

--- src/routes/index.js
+++ src/routes/index.js
@@ -18,27 +18,7 @@ router.post('/auth/singup', [
     check('email', "Email is required").isEmail(),
     check('password', 'Password is requried').isLength({ min: 8 })
 ], auth.userSingup);
-router.delete('/auth/user/:id', passport.authenticate('jwt', {session: false}), function(req, res) {
-    console.log("end point delete user")
-    
-    res.status(200).json({
-        "massage": "this endpoint test version"
-    })
-})
 
-// auth med es
-router.post('/auth/med/singin', [
-    check('email', "Email is required").isEmail(),
-    check('password', 'Password is requried').isLength({ min: 1 })
-], auth.medSingin);
-router.post('/auth/med/singup', [
-    check('lastname').not().isEmpty().withMessage("lastname is required"),
-    check('firstname').not().isEmpty().withMessage("firstname is required"),
-    check('email', "Email is required").isEmail(),
-    check('password', 'Password is requried').isLength({ min: 8 })
-], auth.medSingup);
-
-// 
 
 router.get('/diseses/all', diseases.getAll);
 router.get('/diseses/:id', diseases.getById);
